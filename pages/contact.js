4decbdd2601a2885fde16ebbea37bcd09ad7402c
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2022-03-13 07:37:04 +0500

    origin

--- pages/contact.js
+++ pages/contact.js
@@ -24,7 +24,7 @@ const Contact = () => {
                 <meta name="description" content="" />
                 
             </Head>
-            <Layout>
+            <Layout noShipment={null}>
                 <Hero  /> 
                 <section ref={blue} className={`relative    ${styles.blueBack}`}>
         
